2f47629ecf4b61e64edc0befa5f340007d6db8e0
Nathan Douglas <github@tenesm.us>
2019-06-24 21:01:01 -0500

    Proper bounds checking on map and movement.

--- conatus/examples/basic_movement.rs
+++ conatus/examples/basic_movement.rs
@@ -1,5 +1,6 @@
 /// An experimental roguelike (library), written in Rust.
 extern crate azymus;
+use azymus::component::position::Position;
 use azymus::component::renderable::*;
 
 /// Testing scaffolds for Azymus.
@@ -22,18 +23,20 @@ fn main() {
     trace!("Entering main().");
     let mut root_console = conatus::console::get_root_console(160, 100);
     let mut map_console = conatus::console::get_map_console(160, 95);
-    let mut x = 80;
-    let mut y = 50;
+    let mut position = Position {
+        x: 80,
+        y: 50,
+    };
     while !root_console.window_closed() {
         map_console.clear();
-        map_console.render_renderable(x, y, &Renderable {
+        map_console.render_renderable(position.x, position.y, &Renderable {
             char: Some('@'),
             foreground_color: Some(WHITE),
             background_color: None,
         });
         conatus::console::blit_map_console(&mut map_console, &mut root_console);
         root_console.flush();
-        let exit = conatus::console::handle_keys(&mut root_console, &mut x, &mut y);
+        let exit = conatus::console::handle_keys(&mut root_console, &map_console, &mut position);
         if exit {
             break
         }
--- conatus/src/console/mod.rs
+++ conatus/src/console/mod.rs
@@ -1,3 +1,4 @@
+use azymus::component::position::Position;
 use tcod::console::*;
 
 /// Create a root console with a specified height and width.
@@ -31,7 +32,7 @@ pub fn blit_map_console(map: &mut Offscreen, root: &mut Root) {
 }
 
 /// Handle input.
-pub fn handle_keys(root: &mut Root, x: &mut i32, y: &mut i32) -> bool {
+pub fn handle_keys(root: &mut Root, map: &Offscreen, position: &mut Position) -> bool {
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
     let key = root.wait_for_keypress(true);
@@ -46,22 +47,28 @@ pub fn handle_keys(root: &mut Root, x: &mut i32, y: &mut i32) -> bool {
         }
         Key { code: Escape, .. } => return true, // exit game
         Key { code: Up, .. } => {
-            if *y <= 0 {
+            if position.y <= 0 {
                 return false;
             }
-            *y -= 1
+            position.y -= 1
         },
         Key { code: Down, .. } => {
-            *y += 1
-        },
-        Key { code: Left, .. } => {
-            if *x <= 0 {
+            if position.y >= map.height() - 1 {
                 return false;
             }
-            *x -= 1
+            position.y += 1
+        },
+        Key { code: Left, .. } => {
+            if position.x <= 0 {
+                return false;
+            }
+            position.x -= 1
         },
         Key { code: Right, .. } => {
-            *x += 1
+            if position.x >= map.width() - 1 {
+                return false;
+            }
+            position.x += 1
         },
         _ => {},
     }
