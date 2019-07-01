82f8be922c15f7a7d6ebfc7f9016a760f3023c26
Nathan Douglas <github@tenesm.us>
2019-07-01 17:27:53 -0500

    Part 2: "Offscreen Consoles". See #16.

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -7,14 +7,26 @@ use crate::ui;
 pub fn run() {
     let settings = settings::get_settings();
     let mut root_console = ui::root_console::get_root_console(&settings);
-    let mut x = root_console.width() / 2;
-    let mut y = root_console.height() / 2;
+    let mut map_console = ui::map_console::get_map_console(&settings);
+    let width = map_console.width();
+    let height = map_console.height();
+    let mut x = width / 2;
+    let mut y = height / 2;
     while !root_console.window_closed() {
         use tcod::colors::*;
         use tcod::console::*;
-        root_console.set_default_foreground(WHITE);
-        root_console.clear();
-        root_console.put_char(x, y, '@', BackgroundFlag::None);
+        map_console.set_default_foreground(WHITE);
+        map_console.clear();
+        map_console.put_char(x, y, '@', BackgroundFlag::None);
+        blit(
+            &mut map_console,
+            (0, 0),
+            (width, height),
+            &mut root_console,
+            (0, 0),
+            1.0,
+            1.0,
+        );
         root_console.flush();
         // handle keys and exit game if needed
         let exit = ui::input::handle_keys(&mut root_console, &mut x, &mut y);
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,4 +1,6 @@
 /// Input.
 pub mod input;
+/// The map console.
+pub mod map_console;
 /// The root console.
 pub mod root_console;
