0dd9d599325515b2858238bdbbb1b9f35fa9045f
Nathan Douglas <github@tenesm.us>
2019-07-12 20:05:41 -0500

    Adds hopefully decent calculations of actual compass direction from one point to another.

--- src/math/geometry/compass/mod.rs
+++ src/math/geometry/compass/mod.rs
@@ -1,7 +1,8 @@
+use std::f64::consts::PI;
 use super::cell::CellOffsetType;
 
 /// Compass directions.
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
 pub enum Direction {
     /// North.
     North,
@@ -23,8 +24,25 @@ pub enum Direction {
 
 impl Direction {
 
+    /// From angle.
+    pub fn from_angle(angle: f64) -> Option<Self> {
+        use Direction::*;
+        let index = ((angle + 450.0) % 360.0 / 45.0 ) as usize;
+        let directions = [
+            North,
+            Northeast,
+            East,
+            Southeast,
+            South,
+            Southwest,
+            West,
+            Northwest,
+        ];
+        return Some(directions[index]);
+    }
+
     /// Compass direction corresponding to offset.
-    pub fn from_offset(&self, offset: CellOffsetType) -> Option<Self> {
+    pub fn from_offset(offset: CellOffsetType) -> Option<Self> {
         use Direction::*;
         match offset {
             (-1, -1) => Some(Northwest),
@@ -35,7 +53,10 @@ impl Direction {
             (1, 1) => Some(Southeast),
             (-1, 0) => Some(West),
             (1, 0) => Some(East),
-            _ => None,
+            (dx, dy) => {
+                let angle = (dy as f64).atan2(dx as f64) / PI * 180 as f64;
+                Direction::from_angle(angle)
+            },
         }
     }
 
@@ -52,6 +73,26 @@ impl Direction {
             West => (-1, 0),
             East => (0, -1),
         }
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    /// Ensure our constructor makes sense.
+    #[test]
+    fn from_offset() {
+        assert_eq!(Direction::North, Direction::from_offset((0,-2)).unwrap());
+        assert_eq!(Direction::Northeast, Direction::from_offset((2,-2)).unwrap());
+        assert_eq!(Direction::East, Direction::from_offset((2,0)).unwrap());
+        assert_eq!(Direction::Southeast, Direction::from_offset((2,2)).unwrap());
+        assert_eq!(Direction::South, Direction::from_offset((0,2)).unwrap());
+        assert_eq!(Direction::Southwest, Direction::from_offset((-2,2)).unwrap());
+        assert_eq!(Direction::West, Direction::from_offset((-2,0)).unwrap());
+        assert_eq!(Direction::Northwest, Direction::from_offset((-2,-2)).unwrap());
     }
 
 }
