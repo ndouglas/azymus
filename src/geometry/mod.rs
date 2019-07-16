e17701d08eb0af81b28ca35824fbd6717fbfe056
Nathan Douglas <github@tenesm.us>
2019-07-16 18:53:26 -0500

    Tidy.

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -2,7 +2,6 @@
 
 /// A cell is the smallest addressable block of space within a rectangle.
 pub mod cell;
-/// A chunk is a
 /// A representation of a compass.
 pub mod compass;
 /// A rectangle is a horizontal 2-D array of cells.
